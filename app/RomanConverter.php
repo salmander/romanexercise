1eb2b2d7fbc83dbd73e964304c0a5d783040fce9
salmander <salman.great@gmail.com>
2017-06-29 13:50:24 +0100

    [RN-0001] - Implemented Low interger to single roman converter

--- app/RomanConverter.php
+++ app/RomanConverter.php
@@ -5,11 +5,21 @@ namespace App;
 
 class RomanConverter {
 
+    private static $mapping = [
+        '1' => 'I',
+        '5' => 'V',
+        '10' => 'X',
+        '50' => 'L',
+        '100' => 'C',
+        '500' => 'D',
+        '1000' => 'M',
+    ];
     public static function toRoman(int $number): string
     {
-        $roman = '';
-
-
-        return $roman;
+        foreach (self::$mapping as $key => $value) {
+            if ($key == $number) {
+                return $value;
+            }
+        }
     }
 }
